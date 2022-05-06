e7f98650a4ff723678f0748cce05e222ad88b194
Olivier Guenette <70236191+gocypher42@users.noreply.github.com>
2022-05-06 09:12:22 -0400

    Update null-lsp_cfg.lua

--- nvim/.config/nvim/lua/config/null-lsp_cfg.lua
+++ nvim/.config/nvim/lua/config/null-lsp_cfg.lua
@@ -1,26 +1,26 @@
 local formatting = require("null-ls").builtins.formatting
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 -- Formatter config
 require("null-ls").setup({
 	debug = true,
 	sources = {
-		formatting.tidy.with({
-                	filetypes = { "xml" },
-			extra_args = { "-xml", "-i" },
-             	}),
+		formatting.tidy.with({ extra_args = { "-xml", "-i" } }),
 		formatting.stylua.with({ extra_args = { "--column-width", "80" } }),
 		formatting.black.with({ extra_args = { "-l", "80" } }),
 		formatting.rustfmt.with({
 			extra_args = { "--config", "max_width=80" },
 		}),
 	},
-	on_attach = function(client) -- Format on save
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd([[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-            augroup END
-            ]])
-		end
-	end,
+	on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
 })
