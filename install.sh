9b295d0b6ab6ff318d3ffcb2b91ee98668debbb4
Olivier Guenette <olivierguenette@hotmail.com>
2022-09-28 13:34:23 -0400

    Start working on linux install script

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
-# TODO
+#!/bin/bash
+
+# Declare an array of string with type
+declare -a Packages=( 
+   "git" 
+   "gh"
+   "htop" 
+   "neofetch" 
+   "stow"
+)
+
+test="dnf install -y"
+ 
+# Iterate the string array using for loop
+for val in ${Packages[@]}; do
+   test="$test $val"
+done
+
+dnf update -y
+eval $test
