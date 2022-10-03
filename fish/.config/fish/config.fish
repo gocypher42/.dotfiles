a0512f4ab273ad2a7ed1fa5bf689af6b924042cd
Olivier Guenette <olivierguenette@hotmail.com>
2022-10-03 16:20:20 -0400

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+set -U fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias v=nvim
+alias ls="exa -la --color=always --group-directories-first"
