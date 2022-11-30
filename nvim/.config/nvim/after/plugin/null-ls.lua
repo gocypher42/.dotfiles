c1820bedcbed9e4a30e833f803bfda6ab956fce0
Olivier Guenette <olivier.guenette@marcottesystems.com>
2022-11-30 11:50:18 -0500

    Remove special code for format

--- nvim/.config/nvim/after/plugin/null-ls.lua
+++ nvim/.config/nvim/after/plugin/null-ls.lua
@@ -24,12 +24,13 @@ local sources = {
 }
 
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
+    vim.lsp.buf.format()
+    -- vim.lsp.buf.format({
+    --     filter = function(client)
+    --         return client.name == "null-ls"
+    --     end,
+    --     bufnr = bufnr,
+    -- })
 
     print("Document Formated!")
 end
