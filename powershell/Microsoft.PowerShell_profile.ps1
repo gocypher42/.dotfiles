31749fb92ab75cad1c5080e631734b9ff894b618
Olivier Guenette <olivierguenette@hotmail.com>
2023-02-10 17:12:22 -0500

    Improve powershell setup

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -37,38 +37,16 @@ function touch($file) { "" | Out-File $file -Encoding ascii }
 # =================================
 function update_nvim {
     $nvim_download_link = "https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip"
-    $archive_path = "c:\tmp\nvim-win64.zip"
+    $archive_path = "c:\temp\nvim-win64.zip"
     $tools_path = "c:\tools\neovim\nvim-win64"
 
-    New-Item -Path "c:\" -Name "tmp" -ItemType "directory" -Force
+    New-Item -Path "c:\" -Name "temp" -ItemType "directory" -Force
     Invoke-WebRequest -Uri $nvim_download_link -OutFile $archive_path
     Expand-Archive -Path $archive_path -DestinationPath "c:\tmp" -Force
     Remove-Item -Path $tools_path -Recurse -Force
-    Move-Item -Path "C:\tmp\nvim-win64" -Destination $tools_path
+    Move-Item -Path "C:\temp\nvim-win64" -Destination $tools_path
+    Remove-Item -Path "C:\temp\nvim-win64" -Recurse -Force
     c:\tools\neovim\nvim-win64\bin\nvim.exe --version
-}
-# =================================
-
-# =================================
-# Format PS Scripts
-# =================================
-function ps_format {
-    # Code inspired from 
-    # http://xahlee.info/powershell/powershell_format_script.html
-    Param(
-        [Parameter(Mandatory = $true, Position = 0)]
-        [string]
-        $path
-    )
-
-    if ( Test-Path $path -PathType leaf ) {
-        $fileText = [IO.File]::ReadAllText($path);
-        $formattedOutput = Invoke-Formatter -ScriptDefinition $fileText;
-        Set-Content $path $formattedOutput -NoNewline;
-        Write-Host "Fromat done!";
-        return
-    }
-    Write-Error "file not exist";
 }
 # =================================
 
@@ -79,4 +57,8 @@ function admin {
     } else {
         Start-Process "wt.exe" -Verb runAs
     }
+}
+
+function refreshenv {
+    $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")      
 }
