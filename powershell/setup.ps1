39a4951c0535331b00d49b7f8073afe2b451f65d
Olivier Guenette <olivier.guenette@marcottesystems.com>
2023-02-21 07:36:24 -0500

    update ps setup + profile

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -2,10 +2,11 @@
 
 # Force setup $Profile
 Copy-Item .\Microsoft.PowerShell_profile.ps1 $PROFILE -Force
-. $PROFILE
 
 # Choco install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Oh My Posh install
 winget install JanDeDobbeleer.OhMyPosh -s winget
+
+. $PROFILE
