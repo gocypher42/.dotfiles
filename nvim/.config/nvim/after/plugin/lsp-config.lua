3b1bd765dc91aeb075b294cd79cf397c36118ec6
Olivier Guenette <olivier.guenette@marcottesystems.com>
2024-02-05 09:59:13 -0500

    modify colorscheme to display a black background

--- nvim/.config/nvim/after/plugin/lsp-config.lua
+++ nvim/.config/nvim/after/plugin/lsp-config.lua
@@ -20,7 +20,7 @@ local on_attach = function(_, bufnr)
   
     nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
     nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-    nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+    nmap("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
     nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
     nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
     nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
@@ -53,12 +53,12 @@ local on_attach = function(_, bufnr)
     ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
     ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
   })
-  
+
   -- mason-lspconfig requires that these setup functions are called in this order
   -- before setting up the servers.
   require("mason").setup()
   require("mason-lspconfig").setup()
-  
+
   -- Enable the following language servers
   --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
   --
@@ -73,6 +73,7 @@ local on_attach = function(_, bufnr)
         "clangd",
         "--enable-config",
         "--clang-tidy",
+        "--header-insertion=never",
       },
     },
     -- gopls = {},
@@ -81,7 +82,7 @@ local on_attach = function(_, bufnr)
     tsserver = {},
     -- html = { filetypes = { 'html', 'twig', 'hbs'} },
     lemminx = {},
-  
+
     lua_ls = {
       Lua = {
         workspace = { checkThirdParty = false },
@@ -90,21 +91,21 @@ local on_attach = function(_, bufnr)
     },
   }
   require("clangd_extensions").setup()
-  
+
   -- Setup neovim lua configuration
   require("neodev").setup()
-  
+
   -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-  
+
   -- Ensure the servers above are installed
   local mason_lspconfig = require("mason-lspconfig")
-  
+
   mason_lspconfig.setup({
     ensure_installed = vim.tbl_keys(servers),
   })
-  
+
   mason_lspconfig.setup_handlers({
     function(server_name)
       require("lspconfig")[server_name].setup({
--- nvim/.config/nvim/lua/og/plugins.lua
+++ nvim/.config/nvim/lua/og/plugins.lua
@@ -96,7 +96,14 @@ require("lazy").setup({
     "navarasu/onedark.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("onedark")
+      require('onedark').setup {
+          style = 'darker',
+          transparent = true,
+          lualine = {
+              transparent = true, -- center bar (c) transparency
+          },
+      }
+      require('onedark').load()
     end,
   },
 
