f8a1f375078951318d2e97456255ebb7c21098bc
Olivier Guenette <olivier.guenette@marcottesystems.com>
2024-02-06 07:59:37 -0500

    update poweshell config

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,11 @@
-# =================================
-# OG Custom cfg
-# =================================
-oh-my-posh init pwsh --config "~/.dotfiles/powershell/ohmypush_theme.json" | Invoke-Expression
-# =================================
+# ==================================
+# OG Custom Powerchell Config
+# ==================================
 
-# =================================
-# Custom Alias
-# =================================
+# ==== oh-my-posh ==================
+oh-my-posh init pwsh --config "~/.dotfiles/powershell/ohmypush_theme.json" | Invoke-Expression
+
+# ==== Custom Alias ================
 function v 
 {
     $cmd = "rg --files '.' -g '!*.git\*' | fzf"
@@ -15,51 +14,62 @@ function v
 }
 
 function pushmta { mjs pushmta }
+function rd ($dir) { cmd.exe /c "rd /s /q $dir" }
 
+# === Git Alias =====================
 function gs { git status }
-
 function gb { git branch -l -vv }
-
+function gr { git restore . }
 function lg { lazygit }
 
-function rd ($dir) { cmd.exe /c "rd /s /q $dir" }
-# =================================
+# === CMake Alias ===================
+function cstage { cmake -B build }
+function cdeb { cmake --build build }
+function crel { cmake --build build --config Release }
 
-# =================================
-# Quick functions to switch current direcory
-# =================================
+# === Function for marcotte repos ===
 function repos { Set-Location d:\repos }
-
 function mlang { Set-Location d:\lang\marcotte }
-
 function nvim_cfg { Set-Location $env:USERPROFILE\AppData\Local\nvim }
-
 function msys ($project) { Set-Location -Path D:\Systems\All\$project }
+function touch ($file) 
+{ 
+    "" | Out-File $file -Encoding utf8 
+}
 
-function touch($file) { "" | Out-File $file -Encoding utf8 }
-# =================================
+function mc 
+{ 
+    Param
+    (
+    [string]$file1,
+    [string]$file2,
+    [string]$file3,
+    [string]$file4
+    )
+    $cmd = "cmd /c compile.bat exe $file1 $file2 $file3 $file4"
+    Write-Host $cmd
+    Invoke-Expression -Command $cmd
+}
 
-# =================================
-# Install or update nvim install
-# =================================
+# === Install/Update nvim install ===
 function update_nvim {
     $nvim_download_link = "https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip"
     $module = "nvim-win64"
-    $archive_path = "c:\nvim_temp"
+    $archive_path = "c:\temp"
     $tools_path = "c:\tools\neovim\$module"    
     $zip_file_path = "$archive_path\$module.zip"
     
-    New-Item -Path "c:\" -Name "nvim_temp" -ItemType "directory" -Force
+    New-Item -Path "c:\" -Name "temp" -ItemType "directory" -Force
     Invoke-WebRequest -Uri $nvim_download_link -OutFile $zip_file_path
     Expand-Archive -Path $zip_file_path -DestinationPath $archive_path -Force
     Remove-Item -Path $tools_path -Recurse -Force
     Move-Item -Path "$archive_path\$module" -Destination $tools_path
-    Remove-Item -Path $archive_path -Recurse -Force
-    
-    c:\tools\neovim\nvim-win64\bin\nvim.exe --version
-}
-# =================================
+    Remove-Item -Path $zip_file_path -Recurse -Force
 
+    Invoke-Expression -Command "c:\tools\neovim\nvim-win64\bin\nvim.exe --version"
+}
+
+# === Super User ====================
 function admin 
 {
     if ($args.Count -gt 0) 
@@ -67,10 +77,12 @@ function admin
         $argList = "& '" + $args + "'"
         Start-Process "wt.exe" -Verb runAs -ArgumentList $argList
         return
-    } 
+    }
     Start-Process "wt.exe" -Verb runAs
 }
 
-function refreshenv {
+# === Utils Functions ===============
+function refreshenv 
+{
     $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")      
 }
