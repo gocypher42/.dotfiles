00c73c326af26b75bb567d216479e22369bdd881
Olivier Guenette <70236191+gocypher42@users.noreply.github.com>
2024-08-13 10:07:20 -0400

    Delete fish/.config/fish directory

--- fish/.config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-set -U fish_greeting
-
-alias v=nvim
-alias ls="exa -la --color=always --group-directories-first"
-
-set -U fish_user_paths ~/.cargo/bin $fish_user_paths
-set -U fish_user_paths /usr/lib/jvm/default/bin $fish_user_paths
-set JAVA_HOME /usr/lib/jvm/default
