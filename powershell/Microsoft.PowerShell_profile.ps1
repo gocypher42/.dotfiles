6cfaccf7a002a35f5f4898ffbf293ae31da94ff2
Olivier Guenette <oguenette@commandalkon.com>
2025-02-07 12:27:58 -0500

    Update powershell config

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,108 +1,89 @@
-# ==================================
-# OG Custom Powershell Config
-# ==================================
+#opt-out of telemetry before doing anything, only if PowerShell is run as admin
+if ([bool]([System.Security.Principal.WindowsIdentity]::GetCurrent()).IsSystem) {
+    [System.Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', 'true', [System.EnvironmentVariableTarget]::Machine)
+}
 
-# ==== oh-my-posh ==================
-oh-my-posh init pwsh --config "~/.dotfiles/powershell/ohmypush_config.json" | Invoke-Expression
+# Import Modules and External Profiles
+# Ensure Terminal-Icons module is installed before importing
+if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
+    Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
+}
+Import-Module -Name Terminal-Icons
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
 
-# ==== Custom Alias ================
-function v 
+# Utility Functions
+function Test-CommandExists {
+    param($command)
+    $exists = $null -ne (Get-Command $command -ErrorAction SilentlyContinue)
+    return $exists
+}
+
+function touch($file) { "" | Out-File $file -Encoding ASCII }
+
+# System Utilitie
+function admin {
+    if ($args.Count -gt 0) {
+        $argList = $args -join ' '
+        Start-Process wt -Verb runAs -ArgumentList "pwsh.exe -NoExit -Command $argList"
+    } else {
+        Start-Process wt -Verb runAs
+    }
+}
+
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+    Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+} else {
+    Write-Host "zoxide command not found. Attempting to install via winget..."
+    try {
+        winget install -e --id ajeetdsouza.zoxide
+        Write-Host "zoxide installed successfully. Initializing..."
+        Invoke-Expression (& { (zoxide init powershell | Out-String) })
+    } catch {
+        Write-Error "Failed to install zoxide. Error: $_"
+    }
+}
+
+Set-Alias -Name z -Value __zoxide_z -Option AllScope -Scope Global -Force
+Set-Alias -Name zi -Value __zoxide_zi -Option AllScope -Scope Global -Force
+
+function v
 {
     $cmd = "rg --files '.' -g '!*.git\*' -g '!*.gif' -g '!*.ico' -g '!*.d.*' -g '!*.png' | fzf"
     $selection = Invoke-Expression -Command $cmd
-    if ( $selection ) { nvim $selection }
-}
-
-function jf 
-{
-    $paths = "(Get-ChildItem -Path `"d:\repos`", `"d:\systems\all`", `"d:\dev`" -Directory).FullName"
-    $cmd = "$paths | fzf"
-    $selection = Invoke-Expression -Command $cmd
-    if ( $selection ) { 
-        Set-Location $selection
+    if ( $selection )
+    {
+        nvim $selection
     }
 }
 
-function vjf 
-{
-    jf
-    v
-}
-
-function pushmta { mjs pushmta }
-function rd ($dir) { cmd.exe /c "rd /s /q $dir" }
-function rf ($pattern) { rg --files | rg "$pattern" -i}
-
-# === Git Alias =====================
 function lg { lazygit }
-
 function gs { git status }
 function gb { git branch -l -vv }
-function gr { git restore . }
-function cb {
-    param (
-        [Parameter(Mandatory=$true)]
-        [string] $WI_number,
-        [Parameter(Mandatory=$false)]
-        [string] $source_branch = "master"
-    )
-
-    git switch -c "users/oggy/WI$WI_number" "$source_branch"
-}
-
-# === CMake Alias ===================
-function cstage { cmake -B build }
-function cdeb { cmake --build build }
-function crel { cmake --build build --config Release }
-
-# === Function for marcotte repos ===
-function repos { Set-Location d:\repos }
-function mlang { Set-Location d:\lang\marcotte }
-function nvim_cfg { Set-Location $env:USERPROFILE\AppData\Local\nvim }
-function msys ($project) { Set-Location -Path D:\Systems\All\$project }
-function touch ($file) 
-{ 
-    if (!(Test-Path $file)) {
-        "" | Out-File $file -Encoding utf8 
-    }
-}
 
 # === Install/Update nvim install ===
-function update_nvim {
-    $nvim_download_link = "https://github.com/neovim/neovim/releases/download/stable/nvim-win64.zip"
+function update_nvim
+{
     $module = "nvim-win64"
-    $archive_path = "c:\temp"
-    $tools_path = "c:\tools\neovim\$module"    
-    $zip_file_path = "$archive_path\$module.zip"
-    
-    New-Item -Path "c:\" -Name "temp" -ItemType "directory" -Force
+    $archive = "$module.zip"
+    $nvim_download_link = "https://github.com/neovim/neovim/releases/download/stable/$archive"
+    $tools_path = "c:\tools\neovim\$module"
+    $zip_file_path = "$Env:Temp\$archive"
+
     Invoke-WebRequest -Uri $nvim_download_link -OutFile $zip_file_path
-    Expand-Archive -Path $zip_file_path -DestinationPath $archive_path -Force
+    Expand-Archive -Path $zip_file_path -DestinationPath $ENV:Temp -Force
     Remove-Item -Path $tools_path -Recurse -Force
-    Move-Item -Path "$archive_path\$module" -Destination $tools_path
+    Move-Item -Path "$Env:Temp\$module" -Destination $tools_path
     Remove-Item -Path $zip_file_path -Recurse -Force
 
     Invoke-Expression -Command "c:\tools\neovim\nvim-win64\bin\nvim.exe --version"
 }
 
-# === Super User ====================
-function admin 
+function refreshenv
 {
-    if ($args.Count -gt 0) 
-    {   
-        $argList = "& '" + $args + "'"
-        Start-Process "wt.exe" -Verb runAs -ArgumentList $argList
-        return
-    }
-    Start-Process "wt.exe" -Verb runAs
+    $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 }
 
-# === Utils Functions ===============
-function refreshenv 
-{
-    $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")      
-}
-
-# === Zoxide config ===============
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-function cd { z }
+oh-my-posh init pwsh --config "~/.dotfiles/powershell/ohmyposh_config.json" | Invoke-Expression
