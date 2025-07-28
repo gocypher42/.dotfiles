72a583b0341a386ca410b9a1829a6ce514290e8a
Olivier Guenette <oguenette@commandalkon.com>
2025-07-28 09:57:21 -0400

    update snippets

--- nvim/.config/nvim/after/plugin/luasnip.lua
+++ nvim/.config/nvim/after/plugin/luasnip.lua
@@ -10,12 +10,14 @@ ls.add_snippets("cpp", {
       desc = "Create a C++ main fuction",
     },
     fmt(
-      "#include <iostream>\n"
-        .. "\n"
-        .. "int main(int agrc, char *argv[]) {{\n"
-        .. "  {}\n"
-        .. "  return 0;\n"
-        .. "}}",
+      [[
+      #include <iostream>
+
+      int main(int agrc, char *argv[]) {{
+        {}
+        return 0;
+      }}
+      ]],
       {
         ls.i(0, 'std::cout << "Hello, world!" << std::endl;'),
       }
@@ -31,12 +33,14 @@ ls.add_snippets("python", {
       desc = "Create a Python main fuction",
     },
     fmt(
-      "def main():\n"
-        .. "    {}\n"
-        .. "\n"
-        .. "\n"
-        .. 'if __name__ == "__main__":\n'
-        .. "    main()",
+      [[
+      def main():
+          {}
+
+
+      if __name__ == "__main__":
+          main()
+      ]],
       {
         ls.i(0, 'print("Hello, world!")'),
       }
@@ -52,31 +56,36 @@ ls.add_snippets("cmake", {
       desc = "Generate a default cmake template for a basic project",
     },
     fmt(
-      "cmake_minimum_required(VERSION 3.15)\n"
-        .. "\n"
-        .. "project({}\n"
-        .. "    LANGUAGES CXX\n"
-        .. "    VERSION 1.0\n"
-        .. ")\n"
-        .. "\n"
-        .. "set(CMAKE_CXX_STANDARD 20)\n"
-        .. "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)\n"
-        .. "set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${{CMAKE_BINARY_DIR}}/lib)\n"
-        .. "set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${{CMAKE_BINARY_DIR}}/lib)\n"
-        .. "set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${{CMAKE_BINARY_DIR}}/bin)\n"
-        .. "\n"
-        .. "include(FetchContent)\n"
-        .. "\n"
-        .. "FetchContent_Declare(fmt\n"
-        .. "    GIT_REPOSITORY https://github.com/fmtlib/fmt.git\n"
-        .. "    GIT_TAG 11.2.0\n"
-        .. ")\n"
-        .. "FetchContent_MakeAvailable(fmt)\n"
-        .. "\n"
-        .. "add_executable({} src/main.cpp)\n",
+      [[
+      cmake_minimum_required(VERSION 3.15)
+
+      project(
+        {}
+        LANGUAGES CXX
+        VERSION 1.0
+      )
+
+      set(CMAKE_CXX_STANDARD 20)
+      set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+      set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${{CMAKE_BINARY_DIR}}/lib)
+      set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${{CMAKE_BINARY_DIR}}/lib)
+      set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${{CMAKE_BINARY_DIR}}/bin)
+
+      include(FetchContent)
+
+      FetchContent_Declare(
+        fmt
+        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+        GIT_TAG 11.2.0
+      )
+      FetchContent_MakeAvailable(fmt)
+
+      add_executable({} src/main.cpp{})
+      ]],
       {
-        ls.i(1, "cmake-project"),
+        ls.i(1, "project_name"),
         rep(1),
+        ls.i(0)
       }
     )
   ),
