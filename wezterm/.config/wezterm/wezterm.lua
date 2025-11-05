356bdea281fbecc91984b89ad2c2ed761e592f97
Olivier Guenette <olivierguenette@hotmail.com>
2025-11-05 12:13:54 -0500

    add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,56 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = wezterm.config_builder()
+
+-- font settings
+config.font_size = 12
+config.line_height = 1.2
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+
+-- Cursor
+config.colors = {
+  cursor_bg = "white",
+  cursor_border = "white",
+}
+
+-- Appearance
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.inactive_pane_hsb = {
+  saturation = 0.8,
+  brightness = 0.6,
+}
+config.color_scheme = "Catpuccin Mocha"
+
+-- Miscellaneous settings
+config.max_fps = 120
+config.default_prog = { "pwsh.exe", "-nologo" }
+
+-- Keybinds
+config.leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+  { key = "w", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+
+  -- splits
+  { key = "=", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "-", mods = "ALT|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+  -- moving between panes
+  { key = "LeftArrow", mods = "ALT", action = act.ActivatePaneDirection("Left") },
+  { key = "RightArrow", mods = "ALT", action = act.ActivatePaneDirection("Right") },
+  { key = "UpArrow", mods = "ALT", action = act.ActivatePaneDirection("Up") },
+  { key = "DownArrow", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+
+  -- resize panes
+  { key = "LeftArrow", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Left", 5 }), repeatable = true },
+  { key = "DownArrow", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Down", 5 }), repeatable = true },
+  { key = "UpArrow", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }), repeatable = true },
+  { key = "RightArrow", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Right", 5 }), repeatable = true },
+}
+
+return config
